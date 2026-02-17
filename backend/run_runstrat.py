715cb65db55cd30e65a2a90b31457b0df38fcc99
Kseniya Nigmatulina <kv.nigma@gmail.com>
2026-02-17 14:36:34 +0300

    feat(run_runstrat): run ExecutionTreeContributedCoverage strategy and save artifacts

--- backend/run_runstrat.py
+++ backend/run_runstrat.py
@@ -1,18 +1,27 @@
 import subprocess
+import os
 
 
 def run_runstrat():
+    host_artifacts = os.path.abspath("results")
+    host_launch_info = os.path.abspath("launch_info.csv")
+
     cmd = [
         "docker", "run", "--rm",
+
+        "-v", f"{host_artifacts}:/workspace/PySymGym/tools/runstrat/results",
+        "-v", f"{host_launch_info}:/workspace/PySymGym/tools/runstrat/launch_info.csv",
+
         "pysymgym-test",
 
         "-s", "ExecutionTreeContributedCoverage",
         "-t", "120",
         "-ps", "/workspace/PySymGym",
-        "-sd", "artifacts_run",
+        "-sd", "results/artifacts_run",
         "-as",
-        "/workspace/PySymGym/tools/runstrat/resources/ForTests/bin/Release/net7.0",
-        "/workspace/PySymGym/tools/runstrat/resources/for_tests.csv"
+        "/workspace/PySymGym/maps/DotNet/Maps/Root/bin/Release/net7.0",
+        "/workspace/PySymGym/tools/runstrat/launch_info.csv"
+
     ]
 
     subprocess.run(cmd, check=True)
