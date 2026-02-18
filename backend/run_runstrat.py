6604dcdc9af30f2859ccb8cd2572c0856749d34b
Kseniya Nigmatulina <kv.nigma@gmail.com>
2026-02-18 23:58:14 +0300

    refactor net7 to net8, run runstrat ai strat

--- backend/run_runstrat.py
+++ backend/run_runstrat.py
@@ -4,7 +4,7 @@ import os
 
 def run_runstrat():
     host_artifacts = os.path.abspath("results")
-    host_launch_info = os.path.abspath("launch_info.csv")
+    host_launch_info = os.path.abspath("resources/launch_info.csv")
 
     cmd = [
         "docker", "run", "--rm",
@@ -19,7 +19,7 @@ def run_runstrat():
         "-ps", "/workspace/PySymGym",
         "-sd", "results/artifacts_run",
         "-as",
-        "/workspace/PySymGym/maps/DotNet/Maps/Root/bin/Release/net7.0",
+        "/workspace/PySymGym/maps/DotNet/Maps/Root/bin/Release/net8.0",
         "/workspace/PySymGym/tools/runstrat/launch_info.csv"
 
     ]
@@ -27,4 +27,31 @@ def run_runstrat():
     subprocess.run(cmd, check=True)
 
 
-run_runstrat()
+def run_runstrat_ai():
+    host_artifacts = os.path.abspath("results")
+    host_model_onnx = os.path.abspath("uploads/BCE_model.onnx")
+    host_launch_info = os.path.abspath("resources/launch_info.csv")
+
+    cmd = [
+        "docker", "run", "--rm",
+
+        "-v", f"{host_artifacts}:/workspace/PySymGym/tools/runstrat/results",
+        "-v", f"{host_launch_info}:/workspace/PySymGym/tools/runstrat/launch_info.csv",
+        "-v", f"{host_model_onnx}:/workspace/PySymGym/tools/runstrat/resources/model.onnx",
+
+        "pysymgym-test",
+        "-s", "AI",
+        "-mp", "/workspace/PySymGym/tools/runstrat/resources/model.onnx",
+        "-t", "120",
+        "-ps", "/workspace/PySymGym",
+        "-sd", "results/artifacts_run",
+        "-as",
+        "/workspace/PySymGym/maps/DotNet/Maps/Root/bin/Release/net8.0",
+        "/workspace/PySymGym/tools/runstrat/launch_info.csv"
+
+    ]
+
+    subprocess.run(cmd, check=True)
+
+
+run_runstrat_ai()
