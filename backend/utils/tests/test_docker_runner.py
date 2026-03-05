bc45f7025eb73c0205d1d33e10eca2283d2eccc8
Kseniya Nigmatulina <kv.nigma@gmail.com>
2026-03-05 23:00:54 +0300

    refactor change docker test result dir to pytest tmp_path

--- backend/utils/tests/test_docker_runner.py
+++ backend/utils/tests/test_docker_runner.py
@@ -1,7 +1,5 @@
-import os
-import shutil
-
 import pytest
+from pathlib import Path
 
 from backend.config.paths import (
     ARTIFACTS_AI_CSV_FILE,
@@ -16,18 +14,13 @@ TEST_ARTIFACTS_BASELINE_CSV_FILE = (
     TEST_RESOURCES_DIR + "ExecutionTreeContributedCoverage.csv"
 )
 TEST_ARTIFACTS_AI_CSV_FILE = TEST_RESOURCES_DIR + "AI.csv"
-TEST_RESULTS_DIR = BASE_DIR + "/utils/tests/resources/results"
-
-
-@pytest.fixture(autouse=True)
-def auto_test_results_dir():
-    os.makedirs(TEST_RESULTS_DIR, exist_ok=True)
-    yield
-    shutil.rmtree(TEST_RESULTS_DIR)
 
 
 @pytest.fixture
-def mock_paths(monkeypatch):
+def test_env(tmp_path, monkeypatch):
+    results_dir = tmp_path / "results"
+    results_dir.mkdir()
+
     def mock_get_thread_filepath(uid, filepath):
         if "uploads" in filepath:
             return filepath.replace("tmp/uploads", "utils/tests/resources")
@@ -35,21 +28,29 @@ def mock_paths(monkeypatch):
             return TEST_ARTIFACTS_AI_CSV_FILE
         if filepath == ARTIFACTS_BASELINE_CSV_FILE:
             return TEST_ARTIFACTS_BASELINE_CSV_FILE
+
+        if "results" in filepath:
+            return str(results_dir / Path(filepath).name)
+
         return filepath.replace("tmp", "utils/tests/resources")
 
     monkeypatch.setattr(docker_runner, "get_thread_filepath", mock_get_thread_filepath)
 
+    return {
+        "results_dir": results_dir,
+    }
 
-def test_runstrat_baseline(mock_paths):
+
+def test_runstrat_baseline(test_env):
     uid = "test"
     RunstratBaseline().run(uid)
 
 
-def test_runstrat_ai(mock_paths):
+def test_runstrat_ai(test_env):
     uid = "test"
     RunstratAI().run(uid)
 
 
-def test_compstrat(mock_paths):
+def test_compstrat(test_env):
     uid = "test"
     Compstrat().run(uid)
