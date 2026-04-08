f59f0ab2264b3018bbbf3e64aaa1587d22e7a5be
Kseniya Nigmatulina <kv.nigma@gmail.com>
2026-04-08 11:27:24 +0300

    refactor: remove comma in item condition

--- backend/file_utils/csv_methods_writer.py
+++ backend/file_utils/csv_methods_writer.py
@@ -2,15 +2,14 @@ import csv
 
 
 def write_launch_info_to_csv(
-    *,
-    parsed_methods: list[str],
-    output_file: str,
+        *,
+        parsed_methods: list[str],
+        output_file: str,
 ) -> None:
     with open(output_file, "w") as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(["dll", "method"])
 
         for item in parsed_methods:
-            if "," in item:
-                dll, method = item.split(",", 1)
-                writer.writerow([dll, method])
+            dll, method = item.split(",", 1)
+            writer.writerow([dll, method])
